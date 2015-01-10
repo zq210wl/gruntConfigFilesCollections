d2c4fac4dfb996d6c1fabd59186f08fd55facfd2
zq210wl <zq_210_wl@163.com>
2015-01-10 17:29:50 +0800

    collection files

--- ClarityProduct/Gruntfile - Copy.js
+++ ClarityProduct/Gruntfile - Copy.js
@@ -20,13 +20,6 @@ module.exports = function(grunt) {
       /**** copy work ****/
       copy: {
         development: {
-          options: {
-            enabled: true,
-            max_jshint_notifications: 5, // maximum number of notifications from jshint output
-            title: "Project Name", // defaults to the name in package.json, or will use project directory's name
-            success: false, // whether successful grunt executions should be notified automatically
-            duration: 3 // the duration of notification in seconds, for `notify-send only
-          },
           files: [
               {
                 expand: true,
